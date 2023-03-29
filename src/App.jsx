ef4856f4982f0994f5ee19c3e8fa028478e1b6e1
ujjwal313 <ujjwalkr80@gmail.com>
2023-03-29 15:36:29 +0530

    added react query

--- src/App.jsx
+++ src/App.jsx
@@ -10,207 +10,232 @@ import {
   Flex,
   HStack,
   Image,
-  Select,
+  Spinner,
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
 import axiosConfig from "./axios/index";
 import logo from "./assets/logo.svg";
 import { HiOutlineRefresh } from "react-icons/hi";
 import queryString from "query-string";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const [patientData, setPatientData] = useState({});
-
   const queryParams = queryString.parseUrl(window?.location?.href);
 
   const getUsers = async () => {
     const response = await axiosConfig.get(
       `/order/view?orderId=${queryParams.query.orderId}`
     );
-    console.log(response);
-    setPatientData(response?.data.data);
+    return response?.data.data;
   };
-  useEffect(() => {
-    getUsers();
-  }, []);
+
+  const {
+    data: records,
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["Records"],
+    queryFn: getUsers,
+    refetchInterval: 60000,
+    keepPreviousData: true,
+  });
+
+  const statusOrder = ["pending", "completed"];
+
   return (
     <Box minH="100vh" w="100vw" bg="white" p="24px">
-      <VStack w="100%" textAlign="start" alignItems="center">
-        <Image src={logo} />
-        <VStack w="100%" alignItems="flex-start">
-          <Text fontWeight={700} fontSize="12px" color="#7c7c7c">
-            Patient Name
-          </Text>
-          <Box>
-            <Text fontWeight={400} fontSize="14px">
-              {patientData.first_name + " " + patientData.last_name}
+      {isLoading ? (
+        <Center h="100vh" w="100vw">
+          <Spinner />
+        </Center>
+      ) : (
+        <VStack w="100%" textAlign="start" alignItems="center">
+          <Image src={logo} />
+          <VStack w="100%" alignItems="flex-start">
+            <Text fontWeight={700} fontSize="12px" color="#7c7c7c">
+              Patient Name
             </Text>
-          </Box>
+            <Box>
+              <Text fontWeight={400} fontSize="14px">
+                {records.first_name + " " + records.last_name}
+              </Text>
+            </Box>
+          </VStack>
+          <VStack w="100%" alignItems="flex-start" spacing={0}>
+            <Text fontWeight={700} fontSize="12px" color="#7c7c7c">
+              Lab Name
+            </Text>
+            <Text fontWeight={400} fontSize="14px">
+              HealthCare Clinic
+            </Text>
+          </VStack>
+          <VStack w="100%" alignItems="flex-start" spacing={0}>
+            <Text fontWeight={700} fontSize="12px" color="#7c7c7c">
+              Test Name
+            </Text>
+            <Text fontWeight={400} fontSize="14px">
+              Test 1
+            </Text>
+          </VStack>
+          <HStack w="100%" justifyContent="space-between">
+            <Text>Tests</Text>
+            <Button
+              color="#21B34A"
+              fontWeight={700}
+              fontSize="14px"
+              bg="none"
+              leftIcon={<HiOutlineRefresh />}
+              cursor="pointer"
+              onClick={() => refetch()}
+            >
+              Refresh
+            </Button>
+          </HStack>
+          <Accordion allowToggle defaultIndex={[0]} w="100%">
+            {records?.newData
+              ?.sort(
+                (a, b) =>
+                  statusOrder.indexOf(a.test_status.toLowerCase()) -
+                  statusOrder.indexOf(b.test_status.toLowerCase())
+              )
+              .map((test) => {
+                return (
+                  <AccordionItem my="10px" key={test.test_name}>
+                    <h2>
+                      <AccordionButton bg="#f6f6f6">
+                        <HStack flex={1} justifyContent="space-between">
+                          <VStack spacing={0}>
+                            <Text fontWeight={400} fontSize="14px">
+                              Diagnostic Test
+                            </Text>
+                            <Text
+                              fontWeight={600}
+                              fontSize="12px"
+                              color="#7c7c7c"
+                            >
+                              Queue 2
+                            </Text>
+                          </VStack>
+                          <Center
+                            bg={
+                              test.test_status.toLowerCase() === "completed"
+                                ? "#cce9d4"
+                                : "#f8e4c5"
+                            }
+                            p="4px"
+                            borderRadius="6px"
+                          >
+                            <Text
+                              color={
+                                test.test_status.toLowerCase() === "completed"
+                                  ? "#21B34A"
+                                  : "#FF9900"
+                              }
+                            >
+                              {test.test_status}
+                            </Text>
+                          </Center>
+                        </HStack>
+                        <AccordionIcon />
+                      </AccordionButton>
+                    </h2>
+                    <AccordionPanel
+                      px={0}
+                      boxShadow="0px 8px 24px rgba(0, 0, 0, 0.1)"
+                    >
+                      <VStack
+                        w="100%"
+                        p="16px"
+                        borderRadius="16px"
+                        boxShadow="0px 8px 24px rgba(0, 0, 0, 0.1)"
+                      >
+                        <Flex justifyContent="space-between" w="100%" px="10px">
+                          <VStack alignItems="flex-start">
+                            <Text
+                              alignSelf="flex-start"
+                              fontWeight={700}
+                              fontSize="12px"
+                            >
+                              Test Name
+                            </Text>
+                            <Text>{test.test_name}</Text>
+                          </VStack>
+                          <VStack>
+                            <Text
+                              alignSelf="flex-end"
+                              fontWeight={700}
+                              fontSize="12px"
+                            >
+                              Queue
+                            </Text>
+                            <Text>2</Text>
+                          </VStack>
+                        </Flex>
+                        <Flex justifyContent="space-between" w="100%" px="10px">
+                          <VStack alignItems="flex-start">
+                            <Text
+                              alignSelf="flex-start"
+                              fontWeight={700}
+                              fontSize="12px"
+                            >
+                              Department Name
+                            </Text>
+                            <Text>{test.dept_name || "-"}</Text>
+                          </VStack>
+                          <VStack>
+                            <Text
+                              alignSelf="flex-end"
+                              fontWeight={700}
+                              fontSize="12px"
+                            >
+                              Floor
+                            </Text>
+                            <Text>2</Text>
+                          </VStack>
+                        </Flex>
+                        <Flex justifyContent="space-between" w="100%" px="10px">
+                          <VStack>
+                            <Text
+                              alignSelf="flex-start"
+                              fontWeight={700}
+                              fontSize="12px"
+                            >
+                              Tip
+                            </Text>
+                            <Text>-</Text>
+                          </VStack>
+                          <VStack>
+                            <Text
+                              alignSelf="flex-end"
+                              fontWeight={700}
+                              fontSize="12px"
+                            >
+                              Room
+                            </Text>
+                            <Text>15</Text>
+                          </VStack>
+                        </Flex>
+                        <Flex justifyContent="space-between" w="100%" px="10px">
+                          <VStack>
+                            <Text
+                              alignSelf="flex-start"
+                              fontWeight={700}
+                              fontSize="12px"
+                            >
+                              Status
+                            </Text>
+                            <Text>{test.test_status || "-"}</Text>
+                          </VStack>
+                        </Flex>
+                      </VStack>
+                    </AccordionPanel>
+                  </AccordionItem>
+                );
+              })}
+          </Accordion>
         </VStack>
-        <VStack w="100%" alignItems="flex-start" spacing={0}>
-          <Text fontWeight={700} fontSize="12px" color="#7c7c7c">
-            Lab Name
-          </Text>
-          <Text fontWeight={400} fontSize="14px">
-            HealthCare Clinic
-          </Text>
-        </VStack>
-        <VStack w="100%" alignItems="flex-start" spacing={0}>
-          <Text fontWeight={700} fontSize="12px" color="#7c7c7c">
-            Test Name
-          </Text>
-          <Text fontWeight={400} fontSize="14px">
-            Test 1
-          </Text>
-        </VStack>
-        <HStack w="100%" justifyContent="space-between">
-          <Text>Tests</Text>
-          <Button
-            color="#21B34A"
-            fontWeight={700}
-            fontSize="14px"
-            bg="none"
-            leftIcon={<HiOutlineRefresh />}
-          >
-            Refresh
-          </Button>
-        </HStack>
-        <Accordion allowToggle w="100%">
-          {patientData?.newData?.map((test) => {
-            return (
-              <AccordionItem my="10px">
-                <h2>
-                  <AccordionButton bg="#f6f6f6">
-                    <HStack flex={1} justifyContent="space-between">
-                      <VStack spacing={0}>
-                        <Text fontWeight={400} fontSize="14px">
-                          Diagnostic Test
-                        </Text>
-                        <Text fontWeight={600} fontSize="12px" color="#7c7c7c">
-                          Queue 2
-                        </Text>
-                      </VStack>
-                      <Center bg="#f8e4c5" p="4px" borderRadius="6px">
-                        <Text color="#FF9900">In Progress</Text>
-                      </Center>
-                    </HStack>
-                    <AccordionIcon />
-                  </AccordionButton>
-                </h2>
-                <AccordionPanel
-                  pb={4}
-                  boxShadow="0px 8px 24px rgba(0, 0, 0, 0.1)"
-                >
-                  <VStack
-                    w="100%"
-                    p="16px"
-                    borderRadius="16px"
-                    boxShadow="0px 8px 24px rgba(0, 0, 0, 0.1)"
-                  >
-                    <Flex justifyContent="space-between" w="100%">
-                      <VStack>
-                        <Text
-                          alignSelf="flex-start"
-                          fontWeight={700}
-                          fontSize="12px"
-                        >
-                          Test Name
-                        </Text>
-                        <Text>{test.test_name}</Text>
-                      </VStack>
-                      <VStack>
-                        <Text
-                          alignSelf="flex-end"
-                          fontWeight={700}
-                          fontSize="12px"
-                        >
-                          Queue
-                        </Text>
-                        <Text>2</Text>
-                      </VStack>
-                    </Flex>
-                    <Flex justifyContent="space-between" w="100%">
-                      <VStack>
-                        <Text
-                          alignSelf="flex-start"
-                          fontWeight={700}
-                          fontSize="12px"
-                        >
-                          Department Name
-                        </Text>
-                        <Text>{test.dept_name || "-"}</Text>
-                      </VStack>
-                      <VStack>
-                        <Text
-                          alignSelf="flex-end"
-                          fontWeight={700}
-                          fontSize="12px"
-                        >
-                          Floor
-                        </Text>
-                        <Text>2</Text>
-                      </VStack>
-                    </Flex>
-                    <Flex justifyContent="space-between" w="100%">
-                      <VStack>
-                        <Text
-                          alignSelf="flex-start"
-                          fontWeight={700}
-                          fontSize="12px"
-                        >
-                          Tip
-                        </Text>
-                        <Text>-</Text>
-                      </VStack>
-                      <VStack>
-                        <Text
-                          alignSelf="flex-end"
-                          fontWeight={700}
-                          fontSize="12px"
-                        >
-                          Room
-                        </Text>
-                        <Text>15</Text>
-                      </VStack>
-                    </Flex>
-                    <Flex justifyContent="space-between" w="100%">
-                      <VStack>
-                        <Text
-                          alignSelf="flex-start"
-                          fontWeight={700}
-                          fontSize="12px"
-                        >
-                          Status
-                        </Text>
-                        <Text>{test.test_status || "-"}</Text>
-                      </VStack>
-                    </Flex>
-                  </VStack>
-                </AccordionPanel>
-              </AccordionItem>
-            );
-          })}
-          {/* 
-          <AccordionItem my="10px">
-            <h2>
-              <AccordionButton>
-                <Box as="span" flex="1" textAlign="left">
-                  Section 2 title
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </AccordionPanel>
-          </AccordionItem> */}
-        </Accordion>
-      </VStack>
+      )}
     </Box>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,16 @@ import "./index.css";
 import "@fontsource/poppins/300.css";
 import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/700.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
