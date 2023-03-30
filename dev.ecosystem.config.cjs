3b3ae6362185338bbb1ac497b87b13fd734ebb6e
ujjwal313 <ujjwalkr80@gmail.com>
2023-03-30 09:10:26 +0530

    script changes

--- dev.ecosystem.config.cjs
+++ dev.ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "healjour-fe-dev",
-      script: "npx -y serve -s ~/healjour-fe-dev -l 8000",
+      script: "npx -y serve -s ~/healjour-fe-dev -l 8000 --spa",
     },
   ],
 };
--- master.deploy.sh
+++ master.deploy.sh
@@ -8,12 +8,16 @@ cd ~/healjour-fe-dev
 
 npm i
 
-if npx pm2 ls | grep healjour-fe-dev
+if npx -y pm2 ls | grep healjour-fe-dev
 then
-    npx pm2 del healjour-fe-dev
+    npx -y pm2 save
+    npx -y pm2 del healjour-fe-dev
     cd ~/healjour-fe-dev
-    npx pm2 start dev.ecosystem.config.cjs
+    npx -y pm2 serve ~/healjour-fe-dev/ 8000 --spa
+    npx -y pm2 save
 else
+    npx -y pm2 save
     cd ~/healjour-fe-dev
-    npx pm2 start dev.ecosystem.config.cjs
+    npx -y pm2 serve ~/healjour-fe-dev/ 8000 --spa
+    npx -y pm2 save
 fi
