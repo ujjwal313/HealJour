f3f3006364b07ab34f32438e773d1086cd74dee4
ujjwal313 <ujjwalkr80@gmail.com>
2023-03-30 08:13:07 +0530

    added npx

--- dev.ecosystem.config.cjs
+++ dev.ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "healjour-fe-dev",
-      script: "serve -s ~/healjour-fe-dev -l 8000"
+      script: "npx serve -s ~/healjour-fe-dev -l 8000",
     },
   ],
 };
